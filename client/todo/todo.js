05a6ae77396539b9adcf364a531ce1db11b73792
Niall O'Brien <niall.obrien@clock.co.uk>
2015-06-24 00:27:17 +0100

    > Adds pagination with load more button and loading status indicator

--- client/todo/todo.js
+++ client/todo/todo.js
@@ -1,11 +1,28 @@
+Template.todo.onCreated(function() {
+  var template = this;
+  template.limit = new ReactiveVar(5);
+  template.loaded = new ReactiveVar(0);
+
+  template.todos = function() {
+    return Todos.find({}, { limit: template.loaded.get() });
+  };
+
+});
+
+
 Template.todo.rendered = function () {
+  var template = this;
+
   Vue.use(window['vue-validator']);
   var vm = new Vue({
     el: '#demo',
     data: {
       title: 'todos',
       todos: [],
-      newTodo: ''
+      newTodo: '',
+      limit: template.limit.get(),
+      todosSubscription: null,
+      subscriptionsReady: false
     },
     methods: {
       addTodo: function (e) {
@@ -17,18 +34,33 @@ Template.todo.rendered = function () {
       },
       removeTodo: function (id) {
         Meteor.call('removeTodo', id)
-      }
-    },
-    created: function () {
-      this.subscription = Meteor.subscribe('todos')
+      },
+      loadMore: function() {
+        template.limit.set(this.limit += 5)
+      },
     },
     sync: {
       'todos': function() {
-        return Todos.find();
+        return Todos.find({}, {limit: template.limit.get()});
+      },
+      'todosSubscription': function() {
+        var limit = template.limit.get();
+        var subscription = template.subscribe('todos', limit)
+
+        // if subscription is ready, set limit to new limit
+        if (subscription.ready()) {
+          template.loaded.set(limit);
+          return subscription;
+        }
+      },
+
+      subscriptionsReady: function() {
+        return template.subscriptionsReady();
       }
     },
     destroyed: function () {
       this.subscription.stop()
     }
   });
+
 }
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,5 @@
-Meteor.publish("todos", function () {
-  return Todos.find();
+Meteor.publish("todos", function (limit) {
+  // Simulate slow network response
+  Meteor._sleepForMs(2000);
+  return Todos.find({}, {limit: limit});
 });
