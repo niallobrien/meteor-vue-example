c794f73ed12c72da6f6867054914edfdd295a7b1
Niall O'Brien <niall.obrien@clock.co.uk>
2015-06-24 18:56:26 +0100

    > Minor bugfixes

--- client/todo/todo.js
+++ client/todo/todo.js
@@ -1,19 +1,19 @@
 Template.todo.onCreated(function() {
-  var template = this;
-  template.limit = new ReactiveVar(5);
-  template.loaded = new ReactiveVar(0);
+  var template = this
+  template.limit = new ReactiveVar(5)
+  template.loaded = new ReactiveVar(0)
 
   template.todos = function() {
-    return Todos.find({}, { limit: template.loaded.get() });
-  };
+    return Todos.find({}, {
+      limit: template.limit.get() })
+  }
 
-});
+})
 
+Template.todo.onRendered = function () {
+  var template = this
 
-Template.todo.rendered = function () {
-  var template = this;
-
-  Vue.use(window['vue-validator']);
+  Vue.use(window['vue-validator'])
   var vm = new Vue({
     el: '#demo',
     data: {
@@ -41,26 +41,26 @@ Template.todo.rendered = function () {
     },
     sync: {
       'todos': function() {
-        return Todos.find({}, {limit: template.limit.get()});
+        return Todos.find({}, {limit: template.limit.get()})
       },
       'todosSubscription': function() {
-        var limit = template.limit.get();
+        var limit = template.limit.get()
         var subscription = template.subscribe('todos', limit)
 
         // if subscription is ready, set limit to new limit
         if (subscription.ready()) {
-          template.loaded.set(limit);
-          return subscription;
+          template.loaded.set(limit)
+          return subscription
         }
       },
 
       subscriptionsReady: function() {
-        return template.subscriptionsReady();
+        return template.subscriptionsReady()
       }
     },
     destroyed: function () {
       this.subscription.stop()
     }
-  });
+  })
 
 }
--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
 Meteor.publish("todos", function (limit) {
   // Simulate slow network response
-  Meteor._sleepForMs(2000);
-  return Todos.find({}, {limit: limit});
-});
+  Meteor._sleepForMs(2000)
+  return Todos.find({}, {limit: limit})
+})
