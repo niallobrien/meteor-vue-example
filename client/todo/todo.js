94a47de82e91cc4c24c2918a9bfcfbebf57e6ea9
Niall O'Brien <niall.obrien@clock.co.uk>
2015-06-25 16:34:17 +0100

    > Minor bugfixs

--- client/todo/todo.js
+++ client/todo/todo.js
@@ -1,19 +1,21 @@
 Template.todo.onCreated(function() {
-  var template = this
-  template.limit = new ReactiveVar(5)
-  template.loaded = new ReactiveVar(0)
+  var template = this;
+  template.limit = new ReactiveVar(5);
+  template.loaded = new ReactiveVar(0);
 
   template.todos = function() {
     return Todos.find({}, {
-      limit: template.limit.get() })
-  }
+      limit: template.limit.get()
+    });
+  };
 
-})
+});
+
 
 Template.todo.onRendered = function () {
-  var template = this
+  var template = this;
 
-  Vue.use(window['vue-validator'])
+  Vue.use(window['vue-validator']);
   var vm = new Vue({
     el: '#demo',
     data: {
@@ -36,31 +38,31 @@ Template.todo.onRendered = function () {
         Meteor.call('removeTodo', id)
       },
       loadMore: function() {
-        template.limit.set(this.limit += 5)
+        template.limit.set(template.limit.get() + 5)
       },
     },
     sync: {
       'todos': function() {
-        return Todos.find({}, {limit: template.limit.get()})
+        return Todos.find({}, {limit: template.limit.get()});
       },
       'todosSubscription': function() {
-        var limit = template.limit.get()
+        var limit = template.limit.get();
         var subscription = template.subscribe('todos', limit)
 
         // if subscription is ready, set limit to new limit
         if (subscription.ready()) {
-          template.loaded.set(limit)
-          return subscription
+          template.loaded.set(limit);
+          return subscription;
         }
       },
 
       subscriptionsReady: function() {
-        return template.subscriptionsReady()
+        return template.subscriptionsReady();
       }
     },
     destroyed: function () {
       this.subscription.stop()
     }
-  })
+  });
 
 }
